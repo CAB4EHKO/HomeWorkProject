1c338abd5b15241e1f160c13b2f18eaaf29ed153
Savchenko <savchenko.ra@yandex.ru>
2023-05-10 19:03:01 +0300

    initial commit

---
+++ src/main/java/pro/sky/homeworksavchenko/HomeWorkSavchenkoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworksavchenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkSavchenkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkSavchenkoApplication.class, args);
+	}
+
+}
